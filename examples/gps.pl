0264039b3180dd106af2fced81ba9bb523027e2d
Steve Bertrand <steveb@cpan.org>
2019-11-08 11:00:03 -0800

    added satellite info to gps.pl example

--- examples/gps.pl
+++ examples/gps.pl
@@ -8,8 +8,21 @@ my $gps = GPSD::Parse->new;
 while (1){
     sleep 1;
     next if ! defined $gps->poll;
+
     say $gps->time . "\n";
-    
+
+    my $sats = $gps->satellites;
+
+    my $sat_count = keys %{ $sats };
+
+    my $sats_locked;
+
+    for (keys %$sats){
+        $sats_locked++ if $sats->{$_}{used};
+    }
+
+    say "\tsat: " . $sat_count;    
+    say "\tlck: " . $sats_locked;
     say "\tlat: " . $gps->lat;
     say "\tlon: " . $gps->lon;
     say "\tdeg: " . $gps->track;
